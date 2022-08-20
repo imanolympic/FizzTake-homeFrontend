4c7c6250b232a06ab158a570b683f1143cd6146f
Theron Imanol Mansilla <66397972+imanolympic@users.noreply.github.com>
2022-08-20 16:01:21 -0400

    added navbar skeleton

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import styles from "./NavBar.module.scss";
+
+export const NavBar = () => {
+  const onSubmitRepaymentClick = () => {
+    console.log("submit repayment clicked");
+  };
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}></div>
+
+      <div className={styles.menu}>
+        <Link
+          style={{
+            textDecoration: "none",
+          }}
+          to="/"
+        >
+          <h1> Purchase </h1>
+        </Link>
+
+        <Link
+          style={{
+            textDecoration: "none",
+          }}
+          to="/transactions"
+        >
+          <h1> Past Transactions </h1>
+        </Link>
+
+        <Link
+          style={{
+            textDecoration: "none",
+          }}
+          to="/transactions"
+        >
+          <h1> Past Repayments </h1>
+        </Link>
+
+        <Button variant="outlined" onClick={() => onSubmitRepaymentClick()}>
+          Submit Repayment
+        </Button>
+      </div>
+    </div>
+  );
+};
