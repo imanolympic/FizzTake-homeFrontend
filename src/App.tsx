2264b2446a640efe45a898145e76469842abf14f
Theron Imanol Mansilla <66397972+imanolympic@users.noreply.github.com>
2022-08-20 17:25:20 -0400

    added browser routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { NavBar } from "./components/NavBar/NavBar";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MakePurchasePage } from "./components/MakePurchasePage/MakePurchasePage";
+import TransactionsPage from "./components/TransactionsPage/TransactionsPage";
+import RepaymentsPage from "./components/RepaymentsPage/RepaymentsPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<MakePurchasePage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/repayments" element={<RepaymentsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
